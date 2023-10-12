283ba28294fead50d81e58be3018c57b688948c0
Nisha Singhal <nishasinghal979@gmail.com>
2023-10-12 20:21:52 +0530

    Update text from toDo to notes app

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <section className="text-gray-600 body-font relative">
         <div className="w-full lg:w-4/5 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto mt-10 md:mt-0 relative z-10 shadow-md">
-          <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Add To Do</h2>
+          <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Take a Note</h2>
           <div className="relative mb-4">
             <label htmlFor="email" className="leading-7 text-sm text-gray-600">Title</label>
             <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
