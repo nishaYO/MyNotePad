05a87ec63bb51e684ddbcc98662cf4b9c9fa07be
Nisha Singhal <nishasinghal979@gmail.com>
2023-10-13 14:49:07 +0530

    Add notes UI component in mynotes page

--- app/notes/page.js
+++ app/notes/page.js
@@ -7,7 +7,24 @@ export default function Home() {
     <>
     <Header/>
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      hello notes here 
+    <section class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-wrap -mx-4 -my-8">
+      <div class="py-8 px-4 lg:w-1/3">
+        <div class="h-full flex items-start">
+          <div class="w-12 flex-shrink-0 flex flex-col text-center leading-none">
+            <span class="text-gray-500 pb-2 mb-2 border-b-2 border-gray-200">Jul</span>
+            <span class="font-medium text-lg text-gray-800 title-font leading-none">18</span>
+          </div>
+          <div class="flex-grow pl-6">
+            <h1 class="title-font text-xl font-medium text-gray-900 mb-3">The 400 Blows</h1>
+            <p class="leading-relaxed mb-5">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section> 
     </main>
     <Footer/>
     </>
